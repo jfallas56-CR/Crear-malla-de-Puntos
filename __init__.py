a86a4d6362190b2ab047db0f797a601ac4127b5e
Jorge Fallas <jfallas56@gmail.com>
2026-06-10 18:30:37 -0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Crear Malla de Puntos — Plugin de Processing para QGIS
-Autor: Jorge Fallas (jfallas56@gmail.com)
-"""
-
-def classFactory(iface):
-    from .malla_puntos_plugin import MallaPuntosPlugin
-    return MallaPuntosPlugin(iface)
